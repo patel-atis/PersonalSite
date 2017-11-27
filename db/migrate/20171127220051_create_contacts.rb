c16b79b7039c89f1cc478901e139bbe7c48972cd
Atish <atish.patel95@yahoo.com>
2017-11-27 18:10:55 -0500

    Update database to include name, email, phone

--- db/migrate/20171127220051_create_contacts.rb
+++ db/migrate/20171127220051_create_contacts.rb
@@ -1,7 +1,10 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
-
+      t.string :name
+      t.string :email
+      t.string :phone
+      
       t.timestamps
     end
   end
