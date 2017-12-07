f81ae4d4eef8c14ee835dddfd4355172154fef93
Atish <atish.patel95@yahoo.com>
2017-12-07 13:21:30 -0500

    add routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'extras/me'
+
+  get 'extras/project'
+
+  get 'extras/resume'
+
   root 'contacts#new'
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
