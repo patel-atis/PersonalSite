9eae1c3085243d8c0e30ff42d00a7d8ea228458f
Atish <atish.patel95@yahoo.com>
2017-12-12 20:38:33 -0500

    validate name, email and phone

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 249},
+                    format: { with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+  validates :phone, presence: true, length: {maximum: 10}
+
 end
