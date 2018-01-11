f2dda0af5e36527206a1a337d3f47c9b61d97151
Atish <atish.patel95@yahoo.com>
2018-01-11 17:35:30 -0500

    seed & create languages database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109000923) do
+ActiveRecord::Schema.define(version: 20180111073508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180109000923) do
     t.string "email"
     t.string "phone"
     t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "lang"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+=begin
+languages = Language.create([
+  {lang: "English (Native Tongue)", rating: 5},
+  {lang: "Spanish (Conversational)", rating: 3},
+  {lang: "Gujurati (Passable)", rating: 2},
+  {lang: "French (Juste un peu)", rating: 1}
+  ])
+=end
